e82be22706286cf263d3d7f06892edbedf93000e
Grim <grimhorne@outlook.com>
2021-03-15 22:59:45 -0700

    scaffold starting css and html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div>
+            <label>Site Name</label>
+            <select>
+                <option>Select a site</option>
+                <option>Site 1</option>
+                <option>Site 2</option>
+            </select>
+        </div>
+        <div>
+            <img src="https://picsum.photos/id/537/300/200" alt="" />
+            <div>Add a description of the image here</div>
+        </div>
+        <div>
+            <p>launch date</p>
+            <p>launch site name</p>
+            <p>launch year</p>
+            <p>rocket</p>
+            <p>article</p>
+        </div>
+        <div>
+            <div><img src="https://picsum.photos/id/537/64/64" alt="" /></div>
+            <div><img src="https://picsum.photos/id/537/64/64" alt="" /></div>
+            <div><img src="https://picsum.photos/id/537/64/64" alt="" /></div>
+            <div><img src="https://picsum.photos/id/537/64/64" alt="" /></div>
+        </div>
     </div>
   );
 }
